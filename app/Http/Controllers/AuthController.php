eaf0ca95e7e632f6bb7abaa9bc9b8bed9252fc1f
sickoooo <marianoaelloren@gmail.com>
2025-11-19 19:54:15 +0800

    hi

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth; #AUthentication
+use Illuminate\Support\Facades\Hash; #FOr password
+use App\Models\User;
 class AuthController extends Controller
 {
     function showRegister(){
@@ -13,10 +15,23 @@ class AuthController extends Controller
     function showLogin(){
         return view('authentication.login');
     }
-    function performLogin(){
+    function performLogin(Request $request){
 
     }
-    function performRegister(){
-        
+    function performRegister(Request $request){
+        $request->validate([
+            'name'     => 'required|string|max:255',
+            'email'         => 'required|email|unique:users', 
+            'password'      => 'required|min:6|confirmed'
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return redirect()->route('login.form')->with('success','Registration Successful!');
+
     }
 }
